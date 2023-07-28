ba0027008a6c3fb645f919e7bbc7fa112d8d88d7
eleonora <nebrudko18@gmail.com>
2023-07-28 20:43:33 +0300

    gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
@@ -11,6 +12,7 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+
 );
 
 reportWebVitals();
